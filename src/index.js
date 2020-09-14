14006aea9463b330ead240096a30466af52bcb46
dhmec <dharmoradiya@gmail.com>
2020-09-14 13:08:48 -0600

    add Provider to the render

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { createStore } from 'redux';
+import toDosReducer from './reducers/todos';
+import { addNewToDo } from './actions/todos';
+import { Provider } from 'react-redux';
 
+
+const store = createStore( toDosReducer );
+
+//console.log the store
+store.subscribe( ()=> console.log( store.getState() ) );
+
+store.dispatch( addNewToDo("Research Redux") );
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <> </>
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
